a40e59a029d70e2bd45ecfaef031cc3147466406
Victor Borzaquel <victorborzaquel@outlook.com>
2023-03-27 13:50:13 -0300

    initial commit

---
+++ src/main/java/com/victorborzaquel/whatsprompt/WhatspromptApplication.java
@@ -0,0 +1,13 @@
+package com.victorborzaquel.whatsprompt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatspromptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatspromptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/victorborzaquel/whatsprompt/WhatspromptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorborzaquel.whatsprompt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatspromptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
