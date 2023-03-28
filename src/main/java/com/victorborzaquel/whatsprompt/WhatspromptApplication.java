aa6f8e22320a6eae63773e8d7ef304874e9a9649
Victor Borzaquel <victorborzaquel@outlook.com>
2023-03-28 01:17:34 -0300

    chore: gpt config

--- src/main/java/com/victorborzaquel/whatsprompt/WhatspromptApplication.java
+++ src/main/java/com/victorborzaquel/whatsprompt/WhatspromptApplication.java
@@ -1,9 +1,12 @@
 package com.victorborzaquel.whatsprompt;
 
+import com.victorborzaquel.whatsprompt.config.GptPropertiesConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(GptPropertiesConfig.class)
 public class WhatspromptApplication {
 
     public static void main(String[] args) {
